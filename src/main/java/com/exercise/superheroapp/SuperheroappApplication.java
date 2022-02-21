d9b5e0d8da71031a1f298774d5b1998c542843f7
Aleks <aleksskela591@gmail.com>
2022-02-21 23:27:19 +0100

    init commit

---
+++ src/main/java/com/exercise/superheroapp/SuperheroappApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.superheroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/superheroapp/SuperheroappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.superheroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
